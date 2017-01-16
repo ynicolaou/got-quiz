ed621f7e1e7c0b954c9537c041f5cd75930a4bfd
Yiannis Nicolaou <ynicolaou@rjmetrics.com>
2017-01-16 11:12:28 -0500

    organize into component structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import App from './containers/App'
 
-ReactDOM.render(
-  <App />,
+render(
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
