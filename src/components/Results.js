788fdb00ded70433ad7ab13c5bcb52fdf483d217
Yiannis Nicolaou <ynicolaou@rjmetrics.com>
2017-01-20 01:28:21 -0500

    first pass at displaying results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,14 +1,18 @@
-import React, { PropTypes, Component } from 'react'
+import React, { Component } from 'react'
 
 export default class Results extends Component {
 
-  static propTypes = {
-    score: PropTypes.object.isRequired
-  }
-
   render() {
+    const {img, title, message, scorePercent} = this.props.grade
     return (
-      <span>RESULTS!!!</span>
+      <div>
+        <img src={img}
+             alt={title}
+             className="img-thumbnail"/>
+        <p>{title}</p>
+        <p>{message}</p>
+        <p>{scorePercent}%</p>
+      </div>
     )
   }
 }
--- src/containers/ResultsContainer.js
+++ src/containers/ResultsContainer.js
@@ -2,17 +2,40 @@ import { connect } from 'react-redux'
 import React, { PropTypes } from 'react'
 import Results from '../components/Results'
 
-const ResultsContainer = ({ score }) => (
+const ResultsContainer = ({ grade }) => (
   <Results
-    score={score} />
+    grade={grade} />
 )
 
 ResultsContainer.propTypes = {
-  score: PropTypes.object.isRequired
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  scorePercent: PropTypes.number.isRequired
 }
 
-const mapStateToProps = ({score}) => ({
-  score: score
+const getGrade = (score, grades) => {
+  if(!grades.spec) {
+    return {};
+  }
+
+  let scorePercent = (score.points / score.maxPoints) * 100
+  let grade = grades.spec.results.filter((r) => {
+    return (scorePercent >= r.minpoints &&
+            scorePercent <= r.maxpoints)
+  })[0];
+
+  const {title, message, img} = grade;
+  return {
+    title,
+    message,
+    img,
+    scorePercent
+  };
+}
+
+const mapStateToProps = ({score, grades}) => ({
+  grade: getGrade(score, grades)
 })
 
 export default connect(
